d3affadf2d8c56c68d8e8fe387d60fe6ccbf5d3a
TomomiMatano <>
2024-04-28 16:15:37 +0900

    frontpage作成

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer class="l-footer">
+        <section class="p-footer__top">
+            <ul class="p-footer__top-content">
+                <li>
+                    <a href="/content/company.html">企業情報</a>
+                </li>
+                <li >
+                    <a href="/content/contact.html" class="c-button__footer">お問い合わせ</a>
+                </li>
+            </ul>
+        </section>
+        <section class="p-footer__bottom">
+            <ul class="p-footer__bottom-content">
+                <li>
+                    <img class="c-logo__footer" src="<?php echo esc_url(content_url('/uploads')); ?>/2024/04/logo_kankoujinzai.webp" alt="フッターのロゴ">
+                </li> 
+                <li>
+                    <p class="c-font__footer">千客万来のインバウンド、おもてなし。</p>
+                </li>
+            </ul>
+        </section>
+    </footer>
